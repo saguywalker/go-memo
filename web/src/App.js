820dc9e77b071aba0eb6763cb8647f6e6c1e6c6d
saguywalker <saguywalker@protonmail.com>
2019-11-06 07:14:51 +0700

    stash

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+function Note({ note, index }) {
+  return (
+    <div className="Note">
+      <h4>{note.title}</h4><br />
+      {note.noteDetail}
+    </div>
+  );
+}
+
+function NoteForm({ addNote }) {
+  const [value, setValue] = useState( { title: "", noteDetail: ""} );
+
+  const handleSubmit = e => {
+    console.log(value)
+    e.preventDefault();
+    if (!value.title || !value.noteDetail) return;
+    addNote(value);
+    setValue("", "");
+  };
+
+  return (
+    <form>
+      <input
+        type="text"
+        className="inputTitle"
+        value={value.title}
+        onChange={e => {
+          console.log("onChange(title): ", e.target.value);
+          e.preventDefault();
+          setValue( {title: e.target.value.title} )
+        }}
+      />
+      <input 
+        type="text"
+        className="inputDetail"
+        value={value.noteDetail}
+        onChange={e => {
+          console.log("onChange(detail): ", e.target.value);
+          e.preventDefault();
+          setValue( {noteDetail: e.target.value.noteDetail} )
+        }}
+      />
+      <button onClick={handleSubmit}>Add Note</button>
+    </form>
+  );
+}
 
 function App() {
+  const [notes, setNotes] = useState([
+    {
+      title: "Learn about React",
+      noteDetail: "this is a detail of the first note"
+    },
+    {
+      title: "Meet friend for lunch",
+      noteDetail: "At Tora Yakiniku on 29 Dec, 14:30"
+    },
+    {
+      title: "Build really cool todo app",
+      noteDetail: "It's a good time to learn React"
+    }
+  ]);
+
+  const addNote = (title, noteDetail) => {
+    const newNotes = [...notes, { title, noteDetail }];
+    setNotes(newNotes);
+  };
+
+  const updateNote = (index, title, noteDetail) => {
+    const newNotes = [...notes];
+    newNotes.splice(index, 1, { title, noteDetail })
+    setNotes(newNotes);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Note-list">
+        {notes.map((note, index) => (
+          <Note
+            key={index}
+            index={index}
+            note={note}
+          />
+        ))}
+        <NoteForm 
+          addNote={addNote}
+          updateNote={updateNote}
+        />
+      </div>
     </div>
   );
 }
